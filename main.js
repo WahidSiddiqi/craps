1e22481e6e4818557fe0d1850bfa1cdfde143e1f
WahidSiddiqi <WahidSiddiqi@yahoo.com>
2023-08-07 16:11:42 -0700

    Added HTML content, single event handler, and a little bit of styling

---
+++ main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    const startScreenDiv = document.getElementById("startScreen");
+    const gameScreenDiv = document.getElementById("gameScreen");
+
+    startScreenDiv.style.display = "none";
+    gameScreenDiv.style.display = "block";
+}
