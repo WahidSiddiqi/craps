118bfa7a4c407393e4440d53af84d4bc11b1baf4
WahidSiddiqi <WahidSiddiqi@yahoo.com>
2023-08-08 19:47:14 -0700

    Updated functionality to buttons, added more ids to divs, created functions in javascript file

--- main.js
+++ main.js
@@ -5,3 +5,44 @@ function myFunction() {
     startScreenDiv.style.display = "none";
     gameScreenDiv.style.display = "block";
 }
+
+function updateBankrollHTML() {
+    document.getElementById("bankroll").innerHTML = "Bankroll: $" + currentBankRollValue;
+}
+
+function updateBetHTML() {
+    document.getElementById("bet-value").innerHTML = "Bet: $" + currentBetValue;
+}
+
+
+function betDecrementer() {
+    if (currentBetValue > 100) {
+        currentBetValue -= 100;
+    }
+    updateBetHTML();
+}
+
+function betIncrementer() {
+    if (currentBetValue < currentBankRollValue) {
+        currentBetValue += 100;
+    }
+    updateBetHTML();
+}
+
+function generateRandomDice() {
+    return Math.floor((Math.random() * 6)) + 1;
+}
+
+function rollDice() {
+    dice1 = generateRandomDice();
+    dice2 = generateRandomDice();
+
+}
+
+currentBankRollValue = 1000;
+currentBetValue = 100;
+dice1 = 1;
+dice2 = 1;
+
+updateBankrollHTML();
+updateBetHTML();
